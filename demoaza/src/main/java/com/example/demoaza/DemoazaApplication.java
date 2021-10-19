c947f34d873045660e590c1194186c9af49b9a62
Aza <amangaziev_85@mail.ru>
2021-10-19 14:32:16 +0600

    Init

---
+++ demoaza/src/main/java/com/example/demoaza/DemoazaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoaza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoazaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoazaApplication.class, args);
+	}
+
+}
---
+++ demoaza/src/test/java/com/example/demoaza/DemoazaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoaza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoazaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
